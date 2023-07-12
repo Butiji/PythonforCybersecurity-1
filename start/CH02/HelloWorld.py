c56da42f20d9377a40f3e12f30200ae071f151b2
butiji <butiji.fongwen@student.greenriver.edu>
2023-07-12 00:37:22 -0700

    update

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script 
-# Created by Butiji
-# 11/7/23
-print("Hello World")
+# Created by Butiji, 11/7/2023
+
+your_name = input("What is your name?")
+print("Hello {0}".format(your_name))
+print("Today is going to be a great day!")
